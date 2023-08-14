3d31233b259dc95be03596973ebe4ccb543e8789
sristiM123 <73756057+sristiM123@users.noreply.github.com>
2023-08-15 01:02:47 +0600

    Time: 2678 ms (18.57%), Space: 14.3 MB (37.91%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range (len(nums)):
+            for j in range(i+1, len(nums)):
+                if (i!=j and nums[i] + nums [j] == target) :
+                    return([i,j])
+
+        return []
